9e93359eef1c37ba8f372f1d1cbaf49106379cc9
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2014-02-19 22:17:07 +0100

    preise

--- homoeopathie.php
+++ homoeopathie.php
@@ -10,14 +10,36 @@
 <div class="bild1"><img src="img/homoeopathie1.jpg"/></div>
 <div class="bild2"><img src="img/homoeopathie2.jpg"/></div>
 </div>
+<p>
 Die klassische Homöopathie basiert auf der wissenschaftlichen Arbeit von Dr. Samuel 
 Hahnemann (* 1755 in Meißen; † 1843 in Paris).
 Nach dem Prinzip <i>Ähnliches mit Ähnlichem</i> zu heilen werden Arzneimittel auf pflanzlicher, mineralischer und tierischer Basis verwendet, die im gesunden Organismus in hoher Dosierung Krankheitszustände ähnlich der bestehenden Symptomatik hervorrufen.
 Durch Potenzierung werden die Arzneimittel verdünnt und dynamisiert, so dass sie keine toxische Wirkung mehr auf den Körper haben und die Selbstheilungskräfte im Menschen aktiviert werden.
 Grundlage der Behandlung ist eine genaue Anamnese.
 Sehr wichtig ist auch Ihre  Beobachtung der Symptomatik vor und nach der Mittelgabe. 
-
-
+</p>
+<p>Auch in der Schwangerschaft ist die sanfte Homöopathie ideal!</p>
+<h2>Kosten</h2>
+Die homöopathische Behandlung ist gezielt auf Sie abgestimmt. Nach der Erstanamnese finden wir dabei  sorgfältig und individuell das für Sie und ihr Beschwerdebild passende Mittel. Wir bieten besondere Tarife für Kinder, Jugendliche, Studenten, Arbeitslose, Rentner sowie Sozialtarife an! Bitte kontaktieren Sie uns, um eine individuelle Vereinbarung zu treffen.</br>
+Viele Krankenkassen erstatten die homöopathische Behandlung ganz oder teilweise!
+<br/><br/>
+<table>
+<tr><td>Erstkonsultation Erwachsene</td><td>180,-€</td></tr>
+<tr><td>Jugendliche/ Azubis/ Studenten</td><td>120,-€</td></tr>
+<tr><td>Kinder bis 12 Jahre:		             </td><td>80,-€</td></tr>
+<tr></tr>
+<tr><td>Vorgespräch, wenn erwünscht:			  </td><td>50,-€ (wird angerechnet)</td></tr>
+<tr><td>Folgekonsultationen Erwachsene jeweils:	</td><td>40,- bis 60,-€</td></tr>
+<tr><td>							Kinder:	</td><td>20,-bis 40,-€</td></tr>
+<tr></tr>
+<tr><td>Akutbehandlung:					</td><td>20,- bis 60,-€ je nach Aufwand</td></tr>
+<tr></tr>
+<tr><td>Telefonische Beratung:				 </td><td>5,- bis 30,- € je nach Aufwand</td></tr>
+<tr></tr>
+<tr><td>kurze Information (unter 5 Minuten) </td><td>keine Berechnung</td></tr>
+<tr></tr>
+<tr><td>Hausbesuche:					</td><td>20,- € zusätzlich</td></tr>
+</table>
 </div>
 <?php include 'include/footer.php'; ?>
 </>
